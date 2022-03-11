6091b3ec9f78e0bf9bf351fbf55d785c6c7e48e0
Titus Jungao <johntitusjungao@gmail.com>
2022-03-11 17:27:48 +0800

    Clean main.py

--- app/main.py
+++ app/main.py
@@ -1,31 +1,14 @@
 import streamlit as st
 
 from src.pages import PAGE_MAP
-import pandas as pd
 
-# st.title("Heart Disease Prediction")
-
-# # Side Bar
-# st.sidebar.title("Data Science Pipeline")
-
-# st.sidebar.radio("Go to", 
-# ['Business Understanding',
-#  'Data Understanding', 
-#  'Data Preparation', 
-#  'Modeling', 
-#  'Evaluation', 
-#  'Deployment'])
-
-# df = pd.DataFrame({
-#   'first column': [1, 2, 3, 4],
-#   'second column': [10, 20, 30, 40]
-# })
 
 def main():
     st.sidebar.title("ML Pipeline")
 
     current_page = st.sidebar.radio("Go to", list(PAGE_MAP))
     PAGE_MAP[current_page]().app()
+
 
 if __name__ == "__main__":
     main()
